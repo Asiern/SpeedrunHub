338bbffc410d91d12e7c96601e51e202c3d54e5e
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-03 22:36:21 +0200

    Update App.js

--- Source/App.js
+++ Source/App.js
@@ -1,6 +1,94 @@
-import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import Home from "./app/screens/Home";
+import color from "./app/config/colors";
+
+import * as React from "react";
+import { Text, View, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Constants from "expo-constants";
+
+//ICONS
+import {
+  StarOutlined,
+  StarFilled,
+  StarTwoTone,
+  SafeAreaView,
+  ScrollView,
+  RefreshControl,
+} from "@ant-design/icons";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+//SCROLL
+function wait(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
+function HomeScreen() {
+  return <Home />;
+}
+function AccountScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Account</Text>
+    </View>
+  );
+}
+
+function SearchScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Search</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "ios-list-box" : "ios-list";
+            } else if (route.name === "Search") {
+              iconName = focused ? "ios-search" : "ios-search";
+            } else if (route.name === "Account") {
+              iconName = focused ? "ios-list-box" : "ios-list-box";
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: color.primary,
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Account" component={AccountScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
