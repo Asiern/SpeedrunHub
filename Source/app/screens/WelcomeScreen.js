3ded61ba1c0d88be7655c6edc24aa0146170f904
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-03 15:04:31 +0200

    Create WelcomeScreen.js

---
+++ Source/app/screens/WelcomeScreen.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import colors from "../config/colors";
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      source={require("../assets/background.jpg")}
+      style={styles.backgroung}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>Sell What You Dont Need</Text>
+      </View>
+
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+const styles = StyleSheet.create({
+  backgroung: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logoContainer: {
+    top: 70,
+    position: "absolute",
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.primary,
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: colors.secondary,
+  },
+});
+export default WelcomeScreen;
