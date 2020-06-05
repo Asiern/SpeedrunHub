592192750c231533e1bde2822d05a7861d06d329
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-05 21:56:06 +0200

    Update Home.js

--- Source/app/screens/Home.js
+++ Source/app/screens/Home.js
@@ -5,10 +5,13 @@ import {
   StyleSheet,
   Text,
   SafeAreaView,
+  View,
+  Image,
 } from "react-native";
 import Constants from "expo-constants";
 import color from "../config/colors";
 import GameList from "../components/GameList";
+import colors from "../config/colors";
 
 function wait(timeout) {
   return new Promise((resolve) => {
@@ -27,19 +30,22 @@ export default function Home() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView
-        contentContainerStyle={styles.scrollView}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-      >
-        <ScrollView>
-          <GameList />
-          <GameList />
-          <GameList />
-          <GameList />
-          <GameList />
-        </ScrollView>
+      <View style={styles.banner}>
+        <Image
+          style={styles.logo}
+          source={require("../assets/asiern.png")}
+        ></Image>
+        <Text style={styles.text}>SPEEDRUN.COM</Text>
+      </View>
+      <ScrollView style={styles.scrollView}>
+        <GameList />
+        <GameList />
+        <GameList />
+        <GameList />
+        <GameList />
+        <GameList />
+        <GameList />
+        <GameList />
       </ScrollView>
     </SafeAreaView>
   );
@@ -49,11 +55,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
+    backgroundColor: colors.secondary,
   },
   scrollView: {
     flex: 1,
-    backgroundColor: color.light,
+  },
+  banner: {
+    height: 80,
+    flexDirection: "row",
+    width: "100%",
+    backgroundColor: colors.primary,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-evenly",
+  },
+  text: {
+    color: colors.white,
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  logo: {
+    height: 40,
+    width: 40,
   },
 });
