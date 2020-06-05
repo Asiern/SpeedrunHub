0262de819dc5b4c2119fa69664c7b896f8271657
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-05 21:56:15 +0200

    Update GameList.js

--- Source/app/components/GameList.js
+++ Source/app/components/GameList.js
@@ -1,17 +1,55 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import { StyleSheet, View, Image, Text, ActivityIndicator } from "react-native";
+import config from "../config/config.json";
 import colors from "../config/colors";
 class GameList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    };
+  }
+
+  componentDidMount() {
+    return fetch("https://facebook.github.io/react-native/movies.json")
+      .then((response) => response.json())
+      .then((responseJosn) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJosn.movies,
+        });
+      })
+      .catch((error) => {
+        console.log("APIerror");
+      });
+  }
+
   render() {
-    return <View style={styles.container}></View>;
+    if (this.state.isLoading) {
+      return (
+        <View>
+          <ActivityIndicator />
+        </View>
+      );
+    } else {
+      let movies = this.state.dataSource.map((val, key) => {
+        return (
+          <View style={styles.container} key={key}>
+            <Text>{val.title}</Text>
+          </View>
+        );
+      });
+
+      return <View>{movies}</View>;
+    }
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    paddingTop: 30,
-    backgroundColor: "#ecf0f1",
+    paddingVertical: 20,
+    backgroundColor: colors.light,
     padding: 8,
   },
   paragraph: {
