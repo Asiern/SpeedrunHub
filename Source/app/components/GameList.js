4b0ed24877872c51c36d2841ca37c05c333ce631
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-06 20:31:27 +0200

    Update GameList.js

--- Source/app/components/GameList.js
+++ Source/app/components/GameList.js
@@ -3,6 +3,7 @@ import { StyleSheet, View, Image, Text, ActivityIndicator } from "react-native";
 import config from "../config/config.json";
 import colors from "../config/colors";
 import { color } from "react-native-reanimated";
+import ds from "../assets/json/ds.json";
 class GameList extends Component {
   constructor(props) {
     super(props);
@@ -12,63 +13,69 @@ class GameList extends Component {
     };
   }
 
-  componentDidMount() {
-    return fetch("https://facebook.github.io/react-native/movies.json")
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson.movies,
-        });
-      })
-      .catch((error) => {
-        console.log("APIerror");
-      });
-  }
-
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View>
-          <ActivityIndicator />
-        </View>
-      );
-    } else {
-      let movies = this.state.dataSource.map((val, key) => {
-        return (
-          <View style={styles.container}>
-            <View style={styles.game}>
-              <View key={key}>
-                <Text>{val.title}</Text>
-              </View>
-            </View>
+    return (
+      <View style={styles.container}>
+        <View style={styles.game}>
+          <View style={styles.cover}>
+            <Image
+              style={styles.coverimage}
+              source={{
+                uri: "https://www.speedrun.com/themes/darksouls/cover-128.png",
+              }}
+            ></Image>
           </View>
-        );
-      });
-
-      return <View>{movies}</View>;
-    }
+          <View style={styles.paragraph}>
+            <Text style={styles.title}>{ds.data.names.international}</Text>
+            <Text style={styles.text}>{ds.data["release-date"]}</Text>
+          </View>
+        </View>
+      </View>
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
     backgroundColor: colors.light,
-    padding: 8,
+    padding: 1,
     width: 300,
     alignItems: "center",
     alignSelf: "center",
-    top: 20,
   },
   game: {
     flex: 1,
-    paddingVertical: 40,
-    width: 300,
+    flexDirection: "row",
+    alignContent: "space-between",
+    paddingVertical: 20,
+    paddingLeft: 20,
+    width: 400,
     backgroundColor: colors.white,
     alignItems: "center",
   },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    alignSelf: "center",
+  },
+  text: {
+    alignSelf: "center",
+  },
+  cover: {
+    width: 80,
+    height: 120,
+    alignContent: "center",
+    backgroundColor: colors.secondary,
+    padding: 2,
+  },
+  coverimage: {
+    flex: 1,
+  },
   paragraph: {
-    margin: 24,
+    alignContent: "space-between",
+    flexDirection: "column",
+    flex: 1,
+    height: 120,
+    marginHorizontal: 20,
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
