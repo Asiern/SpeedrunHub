50bf46786434566fa2c09d83ca892c3165146636
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-13 10:28:44 +0200

    Update Home.js

--- Source/app/screens/Home.js
+++ Source/app/screens/Home.js
@@ -1,44 +1,58 @@
-import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { ScrollView, StyleSheet, Text, View, Modal } from "react-native";
 import Constants from "expo-constants";
 import GameCard from "../components/GameCard";
 import User from "../components/User";
 import colors from "../config/colors";
 
-export default function Home() {
-  return (
-    <ScrollView style={styles.container}>
-      <View style={styles.profile}>
-        <User />
-      </View>
-      <View style={{ paddingVertical: 20 }}></View>
-      <Text style={styles.headertext}>Followed Games</Text>
-      <View style={styles.gamelist}>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          <GameCard id={"w6jve26j"} name={"darksouls"} />
-          <GameCard id={"76rkwed8"} name={"na"} />
-          <GameCard id={"o1y5nvdq"} name={"nier"} />
-          <GameCard id={"m1zky010"} name={"darksouls2"} />
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+  }
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Modal visible={this.state.show}></Modal>
+        <ScrollView style={styles.container}>
+          <View style={styles.profile}>
+            <User username={"Asiern"} />
+          </View>
+          <View style={{ paddingVertical: 20 }}></View>
+          <Text style={styles.headertext}>Followed Games</Text>
+          <View style={styles.gamelist}>
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+            >
+              <GameCard id={"w6jve26j"} name={"darksouls"} />
+              <GameCard id={"76rkwed8"} name={"na"} />
+              <GameCard id={"o1y5nvdq"} name={"nier"} />
+              <GameCard id={"m1zky010"} name={"darksouls2"} />
+            </ScrollView>
+          </View>
+          <View style={{ paddingVertical: 20 }}></View>
+          <Text style={styles.headertext}>Popular Games</Text>
+          <View style={styles.populargames}>
+            <View style={styles.gamelist}>
+              <GameCard id={"w6jve26j"} name={"darksouls"} />
+              <GameCard id={"76rkwed8"} name={"na"} />
+              <GameCard id={"o1y5nvdq"} name={"nier"} />
+              <GameCard id={"m1zky010"} name={"darksouls2"} />
+            </View>
+            <View style={styles.gamelist}>
+              <GameCard id={"w6jve26j"} name={"darksouls"} />
+              <GameCard id={"76rkwed8"} name={"na"} />
+              <GameCard id={"o1y5nvdq"} name={"nier"} />
+              <GameCard id={"m1zky010"} name={"darksouls2"} />
+            </View>
+          </View>
         </ScrollView>
       </View>
-      <View style={{ paddingVertical: 20 }}></View>
-      <Text style={styles.headertext}>Popular Games</Text>
-      <View style={styles.populargames}>
-        <View style={styles.gamelist}>
-          <GameCard id={"w6jve26j"} name={"darksouls"} />
-          <GameCard id={"76rkwed8"} name={"na"} />
-          <GameCard id={"o1y5nvdq"} name={"nier"} />
-          <GameCard id={"m1zky010"} name={"darksouls2"} />
-        </View>
-        <View style={styles.gamelist}>
-          <GameCard id={"w6jve26j"} name={"darksouls"} />
-          <GameCard id={"76rkwed8"} name={"na"} />
-          <GameCard id={"o1y5nvdq"} name={"nier"} />
-          <GameCard id={"m1zky010"} name={"darksouls2"} />
-        </View>
-      </View>
-    </ScrollView>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -69,3 +83,5 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
 });
+
+export default Home;
