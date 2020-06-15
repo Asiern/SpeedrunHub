b4d2aa917141c1d349bf84bc47fe9e9ac37d65ed
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-15 12:50:33 +0200

    Create Run.js

---
+++ Source/app/components/Run.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+import { Content, Card, CardItem, Body, Left } from "native-base";
+export default class Run extends Component {
+  render() {
+    return <Content></Content>;
+  }
+}
+module.export = Run;
