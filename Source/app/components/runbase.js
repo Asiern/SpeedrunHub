307b1ab6a934a1ee387a77b9446e705765aec448
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-06-16 19:37:20 +0200

    Create runbase.js

---
+++ Source/app/components/runbase.js
@@ -0,0 +1,8 @@
+<Content>
+  <View style={styles.container}>
+    <Text style={styles.text}>Any% No Wrong Warp</Text>
+    <Text style={styles.text}>1st</Text>
+    <Text style={styles.text}>Regole</Text>
+    <Text style={styles.text}>39m 07s</Text>
+  </View>
+</Content>;
