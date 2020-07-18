3e435c6c9644bc08c898c587f785475c6d6475a9
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-07-18 11:23:27 +0200

    Update Run.js

--- Source/app/components/Run.js
+++ Source/app/components/Run.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Image, Text, Alert } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Alert,
+  ImageBackground,
+} from "react-native";
 import {
   Content,
   Card,
@@ -13,14 +20,23 @@ import { color } from "react-native-reanimated";
 import colors from "../config/colors";
 import { FlatList } from "react-native-gesture-handler";
 export default class Run extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: this.props.category,
+      place: this.props.place,
+      time: this.props.time,
+      runner: this.props.runner,
+    };
+  }
   render() {
     return (
       <Content>
         <View style={styles.container}>
-          <Text style={styles.text}>Any% No Wrong Warp</Text>
-          <Text style={styles.text}>1st</Text>
-          <Text style={styles.text}>Regole</Text>
-          <Text style={styles.text}>39m 07s</Text>
+          <Text style={styles.text}>{this.state.category}</Text>
+          <Text style={styles.text}>{this.state.place}</Text>
+          <Text style={styles.text}>{this.state.runner}</Text>
+          <Text style={styles.text}>{this.state.time}</Text>
         </View>
       </Content>
     );
@@ -28,12 +44,11 @@ export default class Run extends Component {
 }
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 15,
+    paddingVertical: 30,
     flexDirection: "row",
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: colors.white,
+    justifyContent: "space-evenly",
   },
   text: {
     paddingHorizontal: 10,
