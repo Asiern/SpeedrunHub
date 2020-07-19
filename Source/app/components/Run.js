f75542ee5ad3eccdd561e4a1ce046a7317334ef5
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-07-19 10:26:42 +0200

    Update Run.js

--- Source/app/components/Run.js
+++ Source/app/components/Run.js
@@ -1,65 +1,19 @@
 import React, { Component } from "react";
-import {
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  Alert,
-  ImageBackground,
-  TouchableOpacityComponent,
-} from "react-native";
-import {
-  Content,
-  Card,
-  CardItem,
-  Body,
-  Left,
-  List,
-  ListItem,
-} from "native-base";
-import { color } from "react-native-reanimated";
+import { StyleSheet, View, Image, Text } from "react-native";
+import { Content } from "native-base";
 import colors from "../config/colors";
-import { FlatList } from "react-native-gesture-handler";
 export default class Run extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categoryid: this.props.category,
-      category: "",
+      category: this.props.category,
       place: this.props.place,
       time: this.props.time,
       runner: this.props.runner,
       cover: "https://www.speedrun.com/themes/oot/cover-128.png",
-      game: [],
+      game: this.props.game,
       loading: true,
-      abbreviation: "darksouls",
     };
-  }
-  fetchCategory() {
-    fetch("https://www.speedrun.com/api/v1/categories/" + this.state.categoryid)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.state.category = responseJson.data.name;
-      })
-      .catch((error) => {
-        console.log("Data fetching failed");
-      });
-  }
-  async componentDidMount() {
-    const url = "https://www.speedrun.com/api/v1/games/" + this.props.gameid;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    this.setState({
-      loading: false,
-      game: data.data,
-    });
-    //this.setState({ abbreviation: this.state.game.abbreviation.toString() });
-    const coveruri = this.state.game.assets.logo.uri;
-    this.setState({ cover: coveruri });
-  }
-  componentWillMount() {
-    this.fetchCategory();
   }
 
   render() {
@@ -72,6 +26,7 @@ export default class Run extends Component {
           ></Image>
           <Text style={styles.text}>{this.state.category}</Text>
           <Text style={styles.accenttext}>{this.state.place}</Text>
+          <Text style={styles.text}>{this.state.runner}</Text>
           <Text style={styles.text}>{this.state.time}</Text>
         </View>
       </Content>
