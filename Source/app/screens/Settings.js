eeb540e692f0afece6662a2fcefefcbf8cc6e92f
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-07-26 11:57:00 +0200

    Update Settings.js

--- Source/app/screens/Settings.js
+++ Source/app/screens/Settings.js
@@ -1,14 +1,53 @@
 import React, { Component, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import colors from "../config/colors";
+import { Button } from "native-base";
 
 class Settings extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      textinput: "",
+    };
   }
-
+  saveSetting = (input) => {
+    this.textinput = input;
+  };
   render() {
-    return <View style={styles.container}></View>;
+    const textinput = this.state.textinput;
+
+    return (
+      <View style={styles.container}>
+        <Button onPress={this.saveSetting("ASIER")} />
+        <Text>User</Text>
+        <View style={styles.textinputcontainer}>
+          <TextInput
+            placeholder="TEXT"
+            onSubmitEditing={this.saveSetting(textinput)}
+            value={textinput}
+            style={styles.textinput}
+          />
+          <TouchableOpacity
+            style={styles.closeButtonParent}
+            //onPress={() => ()}
+          >
+            <Image
+              style={styles.closeButton}
+              source={{
+                uri: "https://image.flaticon.com/icons/png/512/69/69324.png",
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }
 
@@ -18,17 +57,26 @@ const styles = StyleSheet.create({
     //marginTop: Constants.statusBarHeight,
     backgroundColor: colors.light,
   },
-  user: {
-    flex: 1,
-  },
-  settings: {
-    flex: 4,
-  },
-  input: {
-    margin: 15,
-    height: 40,
-    borderColor: colors.secondary,
+  textinputcontainer: {
+    borderRadius: 5,
     borderWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: colors.primary,
+    borderColor: colors.primary,
+  },
+  textinput: {
+    height: 50,
+    width: "90%",
+  },
+  closeButton: {
+    height: 16,
+    width: 16,
+  },
+  closeButtonParent: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 5,
   },
 });
 
