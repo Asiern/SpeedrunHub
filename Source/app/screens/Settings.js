27eb915ef0bfc58e00a37f133969660d1441fcc0
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-08-04 13:16:23 +0200

    Update Settings.js

--- Source/app/screens/Settings.js
+++ Source/app/screens/Settings.js
@@ -6,45 +6,64 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
+  FlatList
 } from "react-native";
+import Constants from "expo-constants";
+import { SearchBar } from "react-native-elements";
 import colors from "../config/colors";
-import { Button } from "native-base";
+import user from "../config/user.json"
+import User from "../components/User"
+
+
 
 class Settings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      textinput: "",
+      search: "",
+      loading: true,
+      users: [],
+      url: "https://www.speedrun.com/api/v1/users?name=",
     };
   }
-  saveSetting = (input) => {
-    this.textinput = input;
+  updateSearch = (input) => {
+    this.setState({ search: input });
+    const url = "https://www.speedrun.com/api/v1/users?name=" + input;
+    this.Fetch(url);
+    console.log(this.state.users)
+    console.log(url)
+  };
+  async Fetch(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ loading: false, users: data.data });
+  }
+
+  saveJson=(username, userid)=>{
+    user.name = username,
+    user.id = userid
   };
   render() {
-    const textinput = this.state.textinput;
-
+    const { search } = this.state;
     return (
       <View style={styles.container}>
-        <Button onPress={this.saveSetting("ASIER")} />
-        <Text>User</Text>
-        <View style={styles.textinputcontainer}>
-          <TextInput
-            placeholder="TEXT"
-            onSubmitEditing={this.saveSetting(textinput)}
-            value={textinput}
-            style={styles.textinput}
-          />
-          <TouchableOpacity
-            style={styles.closeButtonParent}
-            //onPress={() => ()}
-          >
-            <Image
-              style={styles.closeButton}
-              source={{
-                uri: "https://image.flaticon.com/icons/png/512/69/69324.png",
-              }}
-            />
-          </TouchableOpacity>
+        <SearchBar
+          placeholder="Search for games"
+          onChangeText={this.updateSearch}
+          value={search}
+          platform="ios"
+          lightTheme={true}
+        />
+         <View style={{ flex: 1 }}>
+          <FlatList
+            keyExtractor={(item) => item.id}
+            data={this.state.users}
+            renderItem={({ item }) => (
+              <View style={styles.flatList}>
+                <User username={item.names.international}/>
+              </View>
+            )}
+          ></FlatList>
         </View>
       </View>
     );
@@ -54,8 +73,10 @@ class Settings extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //marginTop: Constants.statusBarHeight,
+    marginTop: Constants.statusBarHeight,
     backgroundColor: colors.light,
+    alignContent:"center",
+    justifyContent:"center"
   },
   textinputcontainer: {
     borderRadius: 5,
