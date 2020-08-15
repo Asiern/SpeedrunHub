bb5263ec0368548e5836813feb0fa4124860c944
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-08-15 11:32:32 +0200

    Update App.js

--- Source/App.js
+++ Source/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import Profile from "./app/screens/Profile";
 import Home from "./app/screens/Home";
 import Settings from "./app/screens/Settings";
+import About from "./app/screens/About";
 import GameInfo from "./app/screens/GameInfo";
 import Search from "./app/screens/Search";
-import User from "./app/components/User";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -30,7 +30,7 @@ export default class App extends Component {
     <Stack.Navigator>
       <Stack.Screen
         name="Profile"
-        component={Profile}
+        component={(Profile, { userid: "48g3q2rx", username: "Asiern" })}
         options={{ title: "My Profile", headerShown: "" }}
       />
       <Stack.Screen name="Game Info" component={GameInfo} />
@@ -62,27 +62,35 @@ export default class App extends Component {
               } else if (route.name === "Home") {
                 iconName = focused ? "md-home" : "md-home";
               } else if (route.name === "Search") {
-                iconName = focused ? "md-search" : "md-search";
+                iconName = focused ? "ios-search" : "ios-search";
               } else if (route.name === "My Games") {
                 iconName = focused ? "md-heart" : "md-heart-empty";
               } else if (route.name === "Streams") {
                 iconName = focused ? "logo-twitch" : "logo-twitch";
+              } else if (route.name === "About") {
+                iconName = focused
+                  ? "ios-information-circle"
+                  : "ios-information-circle-outline";
               }
               return <Ionicons name={iconName} size={size} color={color} />;
             },
           })}
           tabBarOptions={{
-            activeTintColor: colors.Crystalline1,
+            activeTintColor: colors.primary,
             inactiveTintColor: colors.darkgrey,
           }}
         >
           <BottomTabs.Screen name="My Games" children={this.createHomeStack} />
+          {/*
           <BottomTabs.Screen
             name="Profile"
             children={this.createProfileStack}
           />
-          <BottomTabs.Screen name="Search" component={this.createSearchStack} />
           <BottomTabs.Screen name="Settings" component={Settings} />
+          */}
+          <BottomTabs.Screen name="Search" component={this.createSearchStack} />
+
+          <BottomTabs.Screen name="About" component={About} />
         </BottomTabs.Navigator>
       </NavigationContainer>
     );
