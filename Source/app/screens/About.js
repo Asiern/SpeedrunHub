9b9599d870968425928f31d8cceba7b5c05e4743
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-08-15 11:32:51 +0200

    Create About.js

---
+++ Source/app/screens/About.js
@@ -0,0 +1,30 @@
+import React, { Component, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+import colors from "../config/colors";
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ alignSelf: "center" }}>Nothing Here</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    backgroundColor: colors.light,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+});
+
+export default About;
