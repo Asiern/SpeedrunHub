6a26aa530e6f2fdcf42216a5f020b2738ffcfdf2
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-08-15 11:32:48 +0200

    Update colors.js

--- Source/app/config/colors.js
+++ Source/app/config/colors.js
@@ -1,6 +1,6 @@
 export default {
-  primary: "#0F7A4D",
-  secondary: "#262626",
+  primary: "#00cdac",
+  secondary: "#CA39CC",
   Crystalline1: "#00cdac",
   Crystalline2: "#8ddad5",
   light: "#F2F2F2",
