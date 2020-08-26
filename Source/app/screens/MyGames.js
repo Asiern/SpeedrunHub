ad74189102d713ae816811afadade734fc085f4a
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-08-26 12:52:10 +0200

    Settings

--- Source/app/screens/MyGames.js
+++ Source/app/screens/MyGames.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
-export default class Themes extends Component {
+export default class extends Component {
   constructor(props) {
     super(props);
     this.state = {};
