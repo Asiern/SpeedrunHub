01f1fb902435e3ef540ed802aea2cd3163ff0219
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-09-07 08:38:37 +0200

    Update colors.js

--- Source/app/config/colors.js
+++ Source/app/config/colors.js
@@ -1,8 +1,8 @@
 export default {
-  primary: "#ff758c",
+  primary: "#FBC2EB",
   primary2: "#ff758c",
   primaryG: "#ff7eb3",
-  secondary: "#CA39CC",
+  secondary: "#A18CD1",
   Crystalline1: "#00cdac",
   Crystalline2: "#8ddad5",
   light: "#f8f8f8",
@@ -10,4 +10,10 @@ export default {
   grey: "#f0f0f0",
   darkgrey: "#242c37",
   black: "#000",
+  blue: "#4FC3F7",
+  pink: "#F06292",
+  purple: "#BA68C8",
+  orange: "#FF8A65",
+  red: "#e57373",
+  green: "#81C784",
 };
