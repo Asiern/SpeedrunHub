aacb66858b7eaa1a005fbb9953771817d02a7772
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-09-12 20:54:55 +0200

    Added Settings

---
+++ Source/app/screens/Settings/AccountSettings.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AccountSettings = (props) => (
+  <View>
+    <Text>AccountSettings</Text>
+  </View>
+);
+
+export default AccountSettings;
