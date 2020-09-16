088d7704fa05ccf6c6b06cd437b5e208771e29e1
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-09-16 12:57:52 +0200

    Update NotificationBar.js

--- Source/app/components/NotificationBar.js
+++ Source/app/components/NotificationBar.js
@@ -50,15 +50,21 @@ const NotificationBar = (props) => {
   } else {
     return (
       <View style={styles.container}>
-        <TouchableOpacity
-          onPress={() =>
-            props.navigation.navigate("Notifications", {
-              data: data,
-            })
-          }
-        >
-          <Text style={styles.headertext}>Notifications</Text>
-        </TouchableOpacity>
+        <View style={styles.headerConatiner}>
+          <View>
+            <Text style={styles.headertext}>Notifications</Text>
+          </View>
+          <TouchableOpacity
+            onPress={() =>
+              props.navigation.navigate("Notifications", {
+                data: data,
+              })
+            }
+          >
+            <Text style={styles.h2}>View All</Text>
+          </TouchableOpacity>
+        </View>
+
         <FlatList
           keyExtractor={(item) => item.id}
           data={data}
@@ -98,11 +104,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  headerConatiner: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
   headertext: {
     color: colors.darkgrey,
     fontSize: 30,
     marginLeft: 20,
     fontWeight: "bold",
+  },
+  h2: {
+    color: colors.primary,
+    marginRight: 20,
+    fontSize: 20,
   },
 });
 export default NotificationBar;
