147d2b36c9140f1559984953345c7c7cd5c4f5c9
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-09-17 20:58:10 +0200

    Themes

--- Source/app/config/Themes.js
+++ Source/app/config/Themes.js
@@ -3,12 +3,13 @@ export const lightTheme = {
   PRIMARY_BACKGROUND: "#FFF",
   PRIMARY_TEXT: "#242c37",
   PRIMARY_ACCENT: "#4FC3F7",
-  STATUS_BAR_STYILE: "light-content",
+  STATUS_BAR_STYILE: "dark-content",
 };
 export const darkTheme = {
   mode: "dark",
   PRIMARY_BACKGROUND: "#000",
-  PRIMARY_TEXT: "#FFF",
-  PRIMARY_ACCENT: "#4FC3F7",
-  STATUS_BAR_STYILE: "dark-content",
+  SECONDARY_BACKGROUND: "#242c37",
+  PRIMARY_TEXT: "#81C784",
+  PRIMARY_ACCENT: "#81C784",
+  STATUS_BAR_STYILE: "light-content",
 };
--- Source/app/screens/Themes.js
+++ Source/app/screens/Themes.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
 
 import styled, { ThemeConsumer, ThemeProvider } from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { switchTheme } from "../redux/themeActions";
 import { lightTheme, darkTheme } from "../config/Themes";
-import { Button } from "react-native-paper";
 
 export default function Themes() {
   const theme = useSelector((state) => state.themeReducer.theme);
@@ -16,6 +15,7 @@ export default function Themes() {
         {theme.mode === "light" ? (
           <Button
             title={"Switch to Dark"}
+            color={theme.PRIMARY_ACCENT}
             onPress={() => dispatch(switchTheme(darkTheme))}
           >
             Switch to Dark
@@ -23,6 +23,7 @@ export default function Themes() {
         ) : (
           <Button
             title={"Switch to ligth"}
+            color={theme.PRIMARY_ACCENT}
             onPress={() => dispatch(switchTheme(lightTheme))}
           >
             Switch to Light
