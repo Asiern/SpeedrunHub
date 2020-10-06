7ccff3cb9ce676fe53f2fe85338b43776bfea56f
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-10-06 09:43:07 +0200

    Moved ProfileHeader to a component

--- Source/app/screens/Profile.js
+++ Source/app/screens/Profile.js
@@ -1,19 +1,8 @@
 import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  SectionList,
-  Dimensions,
-} from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import { LinearGradient } from "expo-linear-gradient";
-
-import Feather from "@expo/vector-icons/Feather";
-import Constants from "expo-constants";
+import { StyleSheet, View, SectionList, Dimensions } from "react-native";
 
 import colors from "../config/colors";
+import ProfileHeader from "../components/ProfileHeader";
 import PB from "../components/PB";
 import SectionHeader from "../components/SectionHeader";
 
@@ -83,48 +72,6 @@ export default function Profile(props) {
       mounted = false;
     };
   }, []);
-  ProfileHeader = () => {
-    return (
-      <View>
-        <LinearGradient colors={[colors.primary, colors.primary]}>
-          <View style={styles.topbar}>
-            <View style={styles.topbarleft}>
-              <Feather
-                onPress={() => props.navigation.navigate("Home")}
-                name="arrow-left"
-                color={colors.white}
-                size={35}
-                style={{ paddingLeft: 20 }}
-              />
-            </View>
-            <View style={styles.topbarcenter}></View>
-            <View style={styles.topbarright}></View>
-          </View>
-          <View style={styles.imagecontainer}>
-            <Image
-              source={{
-                uri:
-                  "https://www.speedrun.com/themes/user/" +
-                  username +
-                  "/image.png",
-              }}
-              style={styles.Image}
-            ></Image>
-          </View>
-          <View style={styles.userinfo}>
-            <View style={styles.userinfoitem}>
-              <Text style={styles.h1}>{username}</Text>
-              <View style={styles.country}>
-                <View>
-                  <Text style={styles.h2}>{country}</Text>
-                </View>
-              </View>
-            </View>
-          </View>
-        </LinearGradient>
-      </View>
-    );
-  };
 
   ListFooter = () => {
     return <View style={{ padding: 20 }}></View>;
@@ -134,7 +81,13 @@ export default function Profile(props) {
     <SectionList
       sections={sections.data}
       keyExtractor={(item, index) => item + index}
-      ListHeaderComponent={ProfileHeader()}
+      ListHeaderComponent={
+        <ProfileHeader
+          username={username}
+          country={country}
+          navigation={props.navigation}
+        />
+      }
       ListFooterComponent={ListFooter()}
       renderItem={({ item }) => (
         <PB
@@ -157,122 +110,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.light,
-  },
-  topbar: {
-    flex: 1,
-    flexDirection: "row",
-    marginTop: Constants.statusBarHeight,
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-  topbarleft: {
-    flex: 1,
-  },
-  topbarcenter: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  topbarright: {
-    flex: 1,
-  },
-  profile: {
-    flex: 1,
-    paddingBottom: 30,
-    justifyContent: "center",
-    alignItems: "center",
-    alignContent: "center",
-  },
-  imagecontainer: {
-    flex: 1,
-    alignContent: "center",
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  Image: {
-    height: 100,
-    width: 100,
-    borderWidth: 1,
-    borderRadius: 50,
-  },
-  userinfo: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  h1: {
-    color: colors.white,
-    fontSize: 25,
-    fontWeight: "bold",
-    alignSelf: "center",
-  },
-  h2: {
-    color: colors.white,
-    fontSize: 15,
-    fontWeight: "bold",
-    alignSelf: "center",
-  },
-  headertext: {
-    color: colors.light,
-    backgroundColor: colors.Crystalline1,
-    fontSize: 20,
-    fontWeight: "bold",
-    alignSelf: "center",
-    borderRadius: 15,
-    padding: 10,
-  },
-  pbs: {
-    flex: 1,
-    marginHorizontal: 10,
-  },
-  runinfo: {
-    flex: 1,
-    flexDirection: "row",
-    paddingHorizontal: 10,
-    paddingTop: 20,
-  },
-  game: {
-    flex: 3,
-    //backgroundColor: "dodgerblue",
-    alignItems: "center",
-  },
-  category: {
-    flex: 5,
-    //backgroundColor: "gold"
-    alignItems: "center",
-  },
-  place: {
-    flex: 3,
-    //backgroundColor: "tomato",
-    alignItems: "center",
-  },
-  runner: {
-    flex: 5,
-    //backgroundColor: "green",
-    alignItems: "center",
-  },
-  time: {
-    flex: 8,
-    //backgroundColor: "orange",
-    alignItems: "center",
-  },
-  button: {
-    paddingHorizontal: 20,
-    height: 45,
-    alignSelf: "center",
-  },
-  buttontext: {
-    flex: 1,
-    backgroundColor: colors.white,
-    alignContent: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    alignSelf: "center",
-    padding: 10,
-    textAlign: "center",
-    color: colors.darkgrey,
-    fontWeight: "bold",
-    fontSize: 15,
-    borderRadius: 5,
   },
 });
