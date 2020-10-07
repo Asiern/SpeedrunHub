bd50a25572b2c9367a30801350f855e64a884048
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-10-07 21:13:37 +0200

    Navigation Themes

--- Source/app/screens/Navigation.js
+++ Source/app/screens/Navigation.js
@@ -26,123 +26,119 @@ import { useSelector } from "react-redux";
 const Stack = createStackNavigator();
 const BottomTabs = createMaterialBottomTabNavigator();
 
-const theme = useSelector((state) => state.themeReducer.theme);
+export default function Navigation() {
+  const theme = useSelector((state) => state.themeReducer.theme);
 
-class Navigation extends Component {
-  createHomeStack = (props) => (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{ title: "Home", headerShown: "" }}
-      />
-      <Stack.Screen name="Game Info" component={GameInfo} />
-      <Stack.Screen name="Notifications" component={Notifications} />
-      <Stack.Screen
-        name="Profile"
-        component={Profile}
-        options={{ title: "Profile", headerShown: "" }}
-      />
-      <Stack.Screen name="Settings" component={Settings} />
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{ title: "My Profile", headerShown: "" }}
-      />
-      <Stack.Screen name="Themes" component={Themes} />
-    </Stack.Navigator>
-  );
-  createSettingsStack = (props) => (
-    <Stack.Navigator>
-      <Stack.Screen name="Settings" component={Settings} />
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{ title: "My Profile", headerShown: "" }}
-      />
-      <Stack.Screen name="Themes" component={Themes} />
-      <Stack.Screen name="MyGamesSettings" component={MyGamesSettings} />
-      <Stack.Screen
-        name="NotificationsSettings"
-        component={NotificationsSettings}
-        options={{ title: "Notifications Settings" }}
-      />
-      <Stack.Screen
-        name="AccountSettings"
-        component={AccountSettings}
-        options={{ title: "Account Settings" }}
-      />
-      <Stack.Screen name="About" component={About} />
-    </Stack.Navigator>
-  );
-  createSearchStack = (props) => (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Search"
-        component={Search}
-        options={{ title: "Search", headerShown: "" }}
-      />
-      <Stack.Screen name="Game Info" component={GameInfo} />
-      <Stack.Screen
-        name="Profile"
-        component={Profile}
-        options={{ title: "My Profile", headerShown: "" }}
-      />
-    </Stack.Navigator>
-  );
-  render() {
+  function HomeStack(props) {
     return (
-      <ThemeProvider theme={theme}>
-        <Container>
-          <NavigationContainer>
-            <BottomTabs.Navigator
-              initialRouteName="Home"
-              activeColor={colors.primary}
-              inactiveColor={colors.darkgrey}
-              barStyle={{ backgroundColor: colors.white }}
-            >
-              <BottomTabs.Screen
-                name="Home"
-                children={this.createHomeStack}
-                options={{
-                  tabBarLabel: "Home",
-                  tabBarIcon: ({ color }) => (
-                    <Feather name="home" color={color} size={24} />
-                  ),
-                }}
-              />
-              <BottomTabs.Screen
-                name="Search"
-                component={this.createSearchStack}
-                options={{
-                  tabBarLabel: "Search",
-                  tabBarIcon: ({ color }) => (
-                    <Feather name="search" color={color} size={24} />
-                  ),
-                }}
-              />
-
-              <BottomTabs.Screen
-                name="Settings"
-                component={this.createSettingsStack}
-                options={{
-                  tabBarLabel: "Settings",
-                  tabBarIcon: ({ color }) => (
-                    <Feather name="settings" color={color} size={24} />
-                  ),
-                }}
-              />
-            </BottomTabs.Navigator>
-          </NavigationContainer>
-        </Container>
-      </ThemeProvider>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: "Home", headerShown: "" }}
+        />
+        <Stack.Screen name="Game Info" component={GameInfo} />
+        <Stack.Screen name="Notifications" component={Notifications} />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ title: "Profile", headerShown: "" }}
+        />
+      </Stack.Navigator>
     );
   }
+  function SearchStack(props) {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Search"
+          component={Search}
+          options={{ title: "Search", headerShown: "" }}
+        />
+        <Stack.Screen name="Game Info" component={GameInfo} />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ title: "My Profile", headerShown: "" }}
+        />
+      </Stack.Navigator>
+    );
+  }
+  function SettingsStack(props) {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: "My Profile", headerShown: "" }}
+        />
+        <Stack.Screen name="Themes" component={Themes} />
+        <Stack.Screen name="MyGamesSettings" component={MyGamesSettings} />
+        <Stack.Screen
+          name="NotificationsSettings"
+          component={NotificationsSettings}
+          options={{ title: "Notifications Settings" }}
+        />
+        <Stack.Screen
+          name="AccountSettings"
+          component={AccountSettings}
+          options={{ title: "Account Settings" }}
+        />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    );
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>
+        <NavigationContainer>
+          <BottomTabs.Navigator
+            initialRouteName="Home"
+            activeColor={theme.PRIMARY_ACCENT}
+            inactiveColor={theme.INACTIVE}
+            barStyle={{ backgroundColor: theme.PRIMARY_BACKGROUND }}
+          >
+            <BottomTabs.Screen
+              name="Home"
+              children={() => HomeStack()}
+              options={{
+                tabBarLabel: "Home",
+                tabBarIcon: ({ color }) => (
+                  <Feather name="home" color={color} size={24} />
+                ),
+              }}
+            />
+            <BottomTabs.Screen
+              name="Search"
+              component={() => SearchStack()}
+              options={{
+                tabBarLabel: "Search",
+                tabBarIcon: ({ color }) => (
+                  <Feather name="search" color={color} size={24} />
+                ),
+              }}
+            />
+
+            <BottomTabs.Screen
+              name="Settings"
+              component={() => SettingsStack()}
+              options={{
+                tabBarLabel: "Settings",
+                tabBarIcon: ({ color }) => (
+                  <Feather name="settings" color={color} size={24} />
+                ),
+              }}
+            />
+          </BottomTabs.Navigator>
+        </NavigationContainer>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 const Container = styled.View`
   flex: 1;
   background-color: ${(props) => props.theme.SECONDARY_BACKGROUND};
 `;
-
-export default Navigation;
