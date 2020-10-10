900a58922d1b55c90be2108b7870eb06ed7a67cb
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-10-10 09:51:33 +0200

    My Games empty list

--- Source/app/components/MyGames.js
+++ Source/app/components/MyGames.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View,Dimestions, Dimensions } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import GameCard from "./GameCard";
-
+import NotificationCard from "../components/NotificationCard"
+import colors from "../config/colors"
+const {width} = Dimensions.get("screen")
 export default function MyGames(props) {
   return (
     <View style={styles.conatiner}>
@@ -10,6 +12,16 @@ export default function MyGames(props) {
         data={props.data}
         numColumns={3}
         keyExtractor={(item) => item.id}
+        ListEmptyComponent={
+          <NotificationCard  
+            width={width}
+            text={
+              "Start searching for your favourite games and add them to MyGames."
+            }
+            backgroundColor={colors.primary}
+            color={colors.white}
+          />
+      }
         renderItem={({ item }) => (
           <GameCard
             navigation={props.navigation}
