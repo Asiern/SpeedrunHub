3d89af91aef283468d681ae40dcdefa8a98dc0f9
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-10-14 13:35:06 +0200

    Create RunInfo.js

---
+++ Source/app/screens/RunInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const RunInfo = (props) => (
+    <View>
+        <Text>RunInfo</Text>
+    </View>
+);
+
+export default RunInfo;
