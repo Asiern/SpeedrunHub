f35c323bda001537cd89f06b34f906d5579d3341
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-10-26 14:33:31 +0100

    Create Styles.js

---
+++ Source/app/themes/Styles.js
@@ -0,0 +1,32 @@
+import colors from "../config/colors"
+
+export const h1 = {
+    color: colors.darkgrey,
+    fontSize: 30,
+    marginLeft: 20,
+    fontWeight: "bold",
+}
+export const h2 = {
+    color: colors.darkgrey,
+    fontSize: 25,
+    fontWeight: "bold",
+    alignSelf: "center",
+}
+export const h2w = {
+    color: colors.white,
+    fontSize: 25,
+    fontWeight: "bold",
+    alignSelf: "center",
+}
+export const h3 = {
+    color: colors.darkgrey,
+    fontSize: 15,
+    fontWeight: "bold",
+    alignSelf: "center",
+}
+export const h3w = {
+    color: colors.white,
+    fontSize: 15,
+    fontWeight: "bold",
+    alignSelf: "center",
+}
