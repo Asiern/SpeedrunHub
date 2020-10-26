ea9d8ef3f522777df1f77591807f3abe66447f3c
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-10-26 14:33:23 +0100

    Update RunInfo.js

--- Source/app/screens/RunInfo.js
+++ Source/app/screens/RunInfo.js
@@ -1,14 +1,33 @@
-import React,{useEffect} from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React,{ useEffect, useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 
+import { h1, h2 } from "../themes/Styles"
+import VideoPreview from "../components/VideoPreview"
 import colors from "../config/colors"
+
 function getId(weblink){
     const first = weblink.lastIndexOf("/")+1;
     const last = weblink.length;
     return (weblink.slice(first,last));
 }
+function getPlayers(data){
+  var outString = "";
+  for (let player of data.players.data){
+    if(player.names.international != "null"){
+      console.log(player.names.international)
+      outString += player.names.international;
+    }
+    else{
+      outString += player.names.japanese;
+    }
+  }
+  return outString;
+}
 export default function RunInfo (props) {
     const { weblink } = props.route.params;
+    const [data,setData]= useState([]);
+    const [loading,setLoading] = useState(true);
     useEffect(() => {
         let mounted = true;
         if (mounted) {
@@ -17,7 +36,9 @@ export default function RunInfo (props) {
             const url = "https://www.speedrun.com/api/v1/runs/"+getId(weblink)+"?embed=players";
             const response = await fetch(url);
             const data = await response.json();
-            console.log(data)
+            setData(data.data);
+            setLoading(false);
+            console.log(data.data)
             
           })();
         }    
@@ -27,13 +48,36 @@ export default function RunInfo (props) {
       }, []);
     return(
     <View style={styles.container}>
-        <Text>RunInfo</Text>
+        <View style={styles.video}>
+          {loading?
+          <ActivityIndicator size="large" color={colors.blue}/>:  
+          <VideoPreview link={"http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"}/>      
+          }
+        </View>
+        <View style={styles.info}>
+        {loading?<ActivityIndicator size="medium" color={colors.blue}/>:<Text style={h2}>{getPlayers(data)}</Text>}
+        </View>
     </View>);
 }
 
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor: colors.white,
+        backgroundColor: colors.light,
+    },
+    video:{
+      flex:1,
+      backgroundColor: colors.green,
+    },
+    info:{
+      flex:1,
+      margin:20,
+      borderRadius:30,
+      backgroundColor:colors.white,
+      alignItems:"center",
+      shadowColor: "gold",
+      shadowOffset: { width: 5, height: 5 },
+      shadowOpacity: 0.9,
+      elevation: 5,
     }
 })
