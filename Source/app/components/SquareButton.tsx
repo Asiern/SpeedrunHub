e75ed22e92c24a3fbaf28d92c80ca87aec261fc4
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-01 17:15:22 +0100

    Button

---
+++ Source/app/components/SquareButton.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text, ColorValue } from 'react-native';
+import { h3 } from "../themes/Styles"
+
+export interface SquareButtonProps {
+    title: String;
+    color: ColorValue;
+    backgroundColor: ColorValue;
+}
+
+class SquareButtonComponent extends React.Component<SquareButtonProps, any> {
+  constructor(props: SquareButtonProps) {
+    super(props);
+  }
+  
+  public render() {
+    return (
+      <TouchableOpacity style={[styles.container,{ backgroundColor: this.props.backgroundColor }]} >
+         <Text style={[h3,{color: this.props.color}]}>{this.props.title}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 50,
+        width: 200,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: 10,
+        shadowColor: "gold",
+        shadowOffset: { width: 5, height: 5 },
+        shadowOpacity: 0.9,
+        elevation: 5,
+        alignSelf:"center",
+      },
+})
+
+export default SquareButtonComponent;
