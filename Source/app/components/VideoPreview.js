957ba73090f1f8c0ca7add2ece241473080cda26
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-05 18:22:39 +0100

    vulnerabilities fixed

--- Source/app/components/VideoPreview.js
+++ Source/app/components/VideoPreview.js
@@ -1,18 +1,8 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { WebView } from "react-native-webview";
 
 const VideoPreview = ({ link, weblink }) => (
-  <View style={styles.container}>
-    <WebView
-      style={{ marginTop: Platform.OS == "ios" ? 20 : 0 }}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      source={{
-        uri: "https://expo.io",
-      }}
-    />
-  </View>
+  <View style={styles.container}></View>
 );
 const styles = StyleSheet.create({
   container: {
