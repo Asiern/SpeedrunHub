8cc80f1dd1d9f4c22afec29f3ab30350ca5a656f
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-12 09:25:29 +0100

    Update Settings.js
    Removed unused code

--- Source/app/screens/Settings.js
+++ Source/app/screens/Settings.js
@@ -1,13 +1,7 @@
 import React from "react";
-import { StyleSheet, View, TouchableOpacity, Linking } from "react-native";
-import { Text } from "react-native-paper";
-
-import colors from "../config/colors";
-import Feather from "@expo/vector-icons/Feather";
 
 import SettingsSection from "../components/SettingsSection";
 
-import styled, { ThemeProvider } from "styled-components";
 import { useSelector } from "react-redux";
 import { FlatList } from "react-native-gesture-handler";
 
@@ -21,67 +15,31 @@ export default function Settings(props) {
       navigateTo: "AccountSettings",
     },
     {
+      title: "Unlock Pro Version",
+      icon: "lock",
+      navigateTo: "About",
+    },
+    {
       title: "About",
       icon: "info",
       navigateTo: "About",
     },
   ];
   return (
-    <ThemeProvider theme={theme}>
-      <Container>
-        <FlatList
-          data={DATA}
-          renderItem={({ item }) => (
-            <SettingsSection
-              navigateTO={item.navigateTo}
-              title={item.title}
-              icon={item.icon}
-              backgroundColor={theme.PRIMARY_BACKGROUND}
-              accentColor={theme.PRIMARY_ACCENT}
-              textPrimaryColor={theme.PRIMARY_TEXT}
-              navigation={props.navigation}
-            />
-          )}
-          keyExtractor={(item, index) => "key" + index}
-        ></FlatList>
-      </Container>
-    </ThemeProvider>
+    <FlatList
+      data={DATA}
+      renderItem={({ item }) => (
+        <SettingsSection
+          navigateTO={item.navigateTo}
+          title={item.title}
+          icon={item.icon}
+          backgroundColor={theme.PRIMARY_BACKGROUND}
+          accentColor={theme.PRIMARY_ACCENT}
+          textPrimaryColor={theme.PRIMARY_TEXT}
+          navigation={props.navigation}
+        />
+      )}
+      keyExtractor={(item, index) => "key" + index}
+    ></FlatList>
   );
 }
-
-const Container = styled.View`
-  flex: 1;
-  background-color: ${(props) => props.theme.SECONDARY_BACKGROUND};
-`;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.white,
-    alignContent: "center",
-  },
-  buttons: {
-    flex: 1,
-    backgroundColor: colors.light,
-  },
-  button: {
-    padding: 20,
-    flexDirection: "row",
-    backgroundColor: colors.white,
-  },
-  textcontainer: {
-    flex: 3,
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 17,
-    fontWeight: "bold",
-  },
-  icon: {
-    flex: 1,
-    alignContent: "center",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  h2: {},
-});
