d35aa406e039a70af4e716f1e005584a6d72868c
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-12 15:21:51 +0100

    Settings
    Privacy / Terms / Pro

---
+++ Source/app/screens/Settings/Unlock.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function Unlock() {
+  return (
+    <View>
+      <Text>Unlock</Text>
+    </View>
+  );
+}
