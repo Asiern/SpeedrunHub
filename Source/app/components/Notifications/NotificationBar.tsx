1ac87ade36562fd7687b4df1ff76f9df47d09f61
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-16 12:35:01 +0100

    .js -> .tsx

---
+++ Source/app/components/Notifications/NotificationBar.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Animated,
+  FlatList,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import NotificationCard from "./NotificationCard";
+
+import colors from "../../config/colors";
+import { h1, h4p } from "../../themes/Styles";
+import { useNavigation } from "@react-navigation/native";
+
+export interface NotificationBarProps {
+  data: any[];
+  width: string;
+}
+
+const NotificationBar = ({ data, width }: NotificationBarProps) => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerConatiner}>
+        <View>
+          <Text style={[h1, { marginLeft: 20 }]}>Notifications</Text>
+        </View>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("Notifications", {
+              data,
+            })
+          }
+        >
+          <Text style={[h4p, { marginRight: 20 }]}>View All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={data}
+        pagingEnabled
+        renderItem={({ item }) => (
+          <View>
+            {item.status == "read" ? (
+              <Animated.View>
+                <NotificationCard
+                  width={width}
+                  text={item.text}
+                  backgroundColor={colors.white}
+                  color={colors.darkgrey}
+                />
+              </Animated.View>
+            ) : (
+              <Animated.View>
+                <NotificationCard
+                  width={width}
+                  text={item.text}
+                  backgroundColor={colors.primary}
+                  color={colors.white}
+                />
+              </Animated.View>
+            )}
+          </View>
+        )}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+      ></FlatList>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerConatiner: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+export default NotificationBar;
