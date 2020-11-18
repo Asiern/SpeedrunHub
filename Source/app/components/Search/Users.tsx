8a7beb9b09a47e9ef45ba4b051173574fa5f9716
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-18 13:08:26 +0100

    Search tsx

--- Source/app/components/Search/Users.tsx
+++ Source/app/components/Search/Users.tsx
@@ -1,39 +1,37 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
-import GameCard from "./GameCard";
-import { colors } from "../themes/theme";
+import User from "./User";
+import { colors } from "../../themes/theme";
 import { SearchBar } from "react-native-elements";
 
-export default function Games() {
-  const [games, setGames] = useState([]);
+export default function Users() {
+  const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
-
   function updateSearch(input) {
     setSearch(input);
-    const url = "https://www.speedrun.com/api/v1/games?name=" + input;
+    const url = "https://www.speedrun.com/api/v1/users?name=" + input;
     Fetch(url);
   }
   async function Fetch(url) {
     const response = await fetch(url);
     const data = await response.json();
-    setGames(data.data);
+    setUsers(data.data);
   }
+
   return (
     <View style={styles.container}>
       <SearchBar
-        placeholder="Search for games"
+        placeholder="Search for users"
         onChangeText={updateSearch}
         value={search}
         platform="ios"
       />
       <FlatList
-        style={styles.flatList}
-        numColumns={3}
         keyExtractor={(item) => item.id}
-        data={games}
+        data={users}
         renderItem={({ item }) => (
-          <GameCard id={item.id} abbreviation={item.abbreviation} />
+          <User username={item.names.international} userid={item.id} />
         )}
       ></FlatList>
     </View>
@@ -44,11 +42,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.light,
-  },
-  flatList: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    flex: 1,
-    alignSelf: "center",
   },
 });
