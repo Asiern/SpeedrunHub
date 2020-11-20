0a0f0be1cf985eeb683b14477fd1733569a81caa
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-20 13:10:33 +0100

    Splits

---
+++ Source/app/components/Splits/Splits.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import Split from "./Split";
+import { colors, h2 } from "../../themes/theme";
+
+export interface SplitsProps {
+  data?: any[];
+}
+
+export default function Splits({ data }: SplitsProps) {
+  return (
+    <View>
+      <Text style={[h2, { alignSelf: "center", paddingTop: 20 }]}>Splits</Text>
+      <View style={styles.splitsContainer}>
+        {data.map((split, index) => {
+          return (
+            <Split
+              key={index}
+              name={split.name}
+              duration={split.duration}
+              finished={split.finish_time}
+            />
+          );
+        })}
+        <Text style={{ alignSelf: "center" }}>Powered by: splits i/o</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  splitsContainer: {
+    backgroundColor: colors.white,
+    borderRadius: 20,
+    margin: 20,
+    paddingVertical: 20,
+    shadowColor: colors.darkgrey,
+    shadowOffset: { width: 5, height: 5 },
+    shadowOpacity: 0.9,
+    elevation: 2,
+  },
+});
