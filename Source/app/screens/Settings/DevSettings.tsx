c0b5c43c3ef9e018f1911fa141d1c5cca68cab3a
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-22 13:35:59 +0100

    Update DevSettings.tsx

--- Source/app/screens/Settings/DevSettings.tsx
+++ Source/app/screens/Settings/DevSettings.tsx
@@ -1,10 +1,42 @@
+import AsyncStorage from "@react-native-community/async-storage";
 import * as React from "react";
-import { View, Text } from "react-native";
+import { Text, ToastAndroid, View } from "react-native";
+import Button from "../../components/Buttons/Button";
+import { h2 } from "../../themes/theme";
 
 export default function DevSettings() {
+  const Toast = (text: string) => {
+    ToastAndroid.showWithGravity(text, ToastAndroid.SHORT, ToastAndroid.CENTER);
+  };
   return (
-    <View>
-      <Text>DevSettings</Text>
+    <View style={{ alignItems: "center", flex: 1, padding: 20 }}>
+      <Text style={h2}>Async Storage Settings</Text>
+      <Button
+        variant={"primary"}
+        label={"Delete Onboarding"}
+        onPress={async () => {
+          try {
+            await AsyncStorage.removeItem("@Onboarding");
+            Toast("Async Storage deleted");
+          } catch (error) {
+            Toast("Error");
+          }
+        }}
+      />
+      <Button
+        variant={"primary"}
+        label={"Delete MyGames"}
+        onPress={() => async () => {
+          await AsyncStorage.removeItem("@MyGames");
+        }}
+      />
+      <Button
+        variant={"primary"}
+        label={"Delete MyGames"}
+        onPress={() => async () => {
+          await AsyncStorage.removeItem("@MyGames");
+        }}
+      />
     </View>
   );
 }
