b51f8532b58db0630b94eab8a89820255120cfcb
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-11-29 19:28:34 +0100

    Profile fix

--- Source/app/screens/Profile.tsx
+++ Source/app/screens/Profile.tsx
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { View, SectionList } from "react-native";
+import { View, SectionList, ActivityIndicator } from "react-native";
 
 import ProfileHeader from "../components/Profile/ProfileHeader";
 import PB from "../components/PB";
 import SectionHeader from "../components/SectionHeader";
+import { SectionsProps, user } from "../components/Profile/helpers";
 
 import { AdMobBanner } from "expo-ads-admob";
 import { StatusBar } from "expo-status-bar";
 
 export default function Profile(props) {
   const [country, setCountry] = useState("");
-  const [sections, setSections] = useState({});
-  const [user, setUser] = useState([]);
+  const [sections, setSections] = useState<SectionsProps>();
+  const [user, setUser] = useState<user>();
   const { username, userid } = props.route.params;
 
   function filterPBS(data) {
-    var sectionList = {
+    var sectionList: SectionsProps = {
       data: [], //sections
       pagination: [],
     };
+
     for (let run of data) {
+      var counter = 0;
       var section = {
         abbreviation: "",
         name: "",
@@ -36,12 +39,14 @@ export default function Profile(props) {
       }
       //Create run object
       var r = {
+        key: counter.toString(),
         place: run.place,
         runnerid: run.run.players[0].id,
         time: run.run.times.primary,
         category: run.category.data.name,
         weblink: run.run.weblink,
       };
+      counter++;
       //Push data
       var index = sectionList.pagination.indexOf(run.game.data.id);
       sectionList.data[index].data.push(r);
@@ -77,44 +82,48 @@ export default function Profile(props) {
     };
   }, []);
 
-  return (
-    <>
-      <StatusBar style={"dark"}></StatusBar>
-      <SectionList
-        sections={sections.data}
-        keyExtractor={(item, index) => item + index}
-        ListFooterComponent={
-          <View style={{ paddingTop: 20 }}>
-            <AdMobBanner
-              bannerSize="fullBanner"
-              adUnitID="ca-app-pub-3552758561036628/7487974176"
-              servePersonalizedAds
+  if (sections == undefined) {
+    return <ActivityIndicator />;
+  } else {
+    return (
+      <>
+        <StatusBar style={"dark"}></StatusBar>
+        <SectionList
+          sections={sections.data}
+          keyExtractor={(item, index) => item.key + index}
+          ListFooterComponent={
+            <View style={{ paddingTop: 20 }}>
+              <AdMobBanner
+                bannerSize="fullBanner"
+                adUnitID="ca-app-pub-3552758561036628/7487974176"
+                servePersonalizedAds
+              />
+            </View>
+          }
+          ListHeaderComponent={
+            <ProfileHeader
+              username={username}
+              country={country}
+              signup={user.signup}
             />
-          </View>
-        }
-        ListHeaderComponent={
-          <ProfileHeader
-            username={username}
-            country={country}
-            signup={user.signup}
-          />
-        }
-        renderItem={({ item }) => (
-          <PB
-            place={item.place}
-            time={item.time}
-            category={item.category}
-            weblink={item.weblink}
-          />
-        )}
-        renderSectionHeader={({ section }) => (
-          <SectionHeader
-            abbreviation={section.abbreviation}
-            id={section.id}
-            name={section.name}
-          />
-        )}
-      />
-    </>
-  );
+          }
+          renderItem={({ item }) => (
+            <PB
+              place={item.place}
+              time={item.time}
+              category={item.category}
+              weblink={item.weblink}
+            />
+          )}
+          renderSectionHeader={({ section }) => (
+            <SectionHeader
+              abbreviation={section.abbreviation}
+              id={section.id}
+              name={section.name}
+            />
+          )}
+        />
+      </>
+    );
+  }
 }
