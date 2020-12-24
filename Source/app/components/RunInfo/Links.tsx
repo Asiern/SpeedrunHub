30f05cab2bbfd96b176655993dfa291e48ec6949
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-12-24 18:03:41 +0100

    updated: RunInfo, VideoCarousel+ created: Links, Modal, Runners, TopBar +

---
+++ Source/app/components/RunInfo/Links.tsx
@@ -0,0 +1,66 @@
+import { Feather } from "@expo/vector-icons";
+import * as React from "react";
+import { View, StyleSheet, Linking, Share } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { colors } from "../../themes/theme";
+import { GLYPHS } from "@expo/vector-icons/createIconSet";
+
+export interface LinksProps {
+  videolink: string;
+  weblink: string;
+}
+
+export default function Links({ videolink, weblink }: LinksProps) {
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message: weblink,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  //Loads videolink on browser
+  function openOnBrowser(link: string) {
+    Linking.openURL(link);
+  }
+
+  //Set icon depending on the video platform
+  var browsericon: GLYPHS = "globe";
+  if (videolink.includes("yout")) {
+    browsericon = "youtube";
+  } else if (videolink.includes("twit")) {
+    browsericon = "twitch";
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => openOnBrowser(videolink)}>
+        <Feather name={browsericon} size={30} color={colors.white} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onShare}>
+        <Feather name={"share-2"} size={30} color={colors.white} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    padding: 10,
+    justifyContent: "space-around",
+    backgroundColor: colors.primary,
+  },
+});
--- Source/app/screens/RunInfo.tsx
+++ Source/app/screens/RunInfo.tsx
@@ -1,20 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
-import { ActivityIndicator } from "react-native-paper";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 
-import User from "../components/Search/User";
-
-import { colors, h4 } from "../themes/theme";
-import { FlatList } from "react-native-gesture-handler";
+import { colors, h4, shadow } from "../themes/theme";
 import Splits from "../components/Splits/Splits";
 import VideoCarousel from "../components/RunInfo/VideoCarousel";
+import GameCard from "../components/GameCard";
+import TopBar from "../components/TopBar";
+import Links from "../components/RunInfo/Links";
+import Modal from "../components/RunInfo/Modal";
+import Runners from "../components/RunInfo/Runners";
 
 //Interface
 import { run } from "../interface/runInterface";
 import { game } from "../interface/gameInterface";
 import { category } from "../interface/categoryInterface";
 import { player } from "../interface/playersInterface";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
 
 interface dataProps {
   game: {
@@ -28,24 +39,29 @@ interface dataProps {
   };
 }
 
-const { width } = Dimensions.get("window");
-
 function getId(weblink: string) {
   const first = weblink.lastIndexOf("/") + 1;
   const last = weblink.length;
   return weblink.slice(first, last);
 }
 function getPlayers(data: run & dataProps) {
-  var outString = "";
+  var runners = [];
   for (let player of data.players.data) {
+    var runner = {
+      userid: "",
+      username: "",
+    };
+    runner.userid = player.id;
     if (player.names.international != "null") {
-      outString += player.names.international;
+      runner.username = player.names.international;
     } else {
-      outString += player.names.japanese;
+      runner.username = player.names.japanese;
     }
+    runners.push(runner);
   }
-  return outString;
+  return runners;
 }
+
 function timeConverter(time: string) {
   var result = time.toLowerCase();
   return result.substr(2, result.length);
@@ -54,10 +70,20 @@ function timeConverter(time: string) {
 export default function RunInfo(props) {
   const { weblink } = props.route.params;
   const [data, setData] = useState<dataProps & run>();
-  const [splits, setSplits] = useState([]);
+  //const [splits, setSplits] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [runners, setRunners] = useState([]);
-
+  const scroll = useRef<Animated.ScrollView>(null);
+  const ScrollY = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: ({ contentOffset }) => {
+      ScrollY.value = contentOffset.y;
+    },
+  });
+  //Toggle modal visible
+  function onPress() {
+    setModalVisible(!modalVisible);
+  }
   useEffect(() => {
     let mounted = true;
     if (mounted) {
@@ -67,18 +93,17 @@ export default function RunInfo(props) {
           "https://www.speedrun.com/api/v1/runs/" +
           getId(weblink) +
           "?embed=players,category,game";
-        console.log(url);
+        //console.log(url);
         const response = await fetch(url);
         const data = await response.json();
         setData(data.data);
-        setRunners(data.data.players.data);
         //Splits
-        if (data.data.splits != null) {
-          const splitUrl = data.data.splits.uri;
-          const splitresponse = await fetch(splitUrl);
-          const splitdata = await splitresponse.json();
-          setSplits(splitdata.run.splits);
-        }
+        // if (data.data.splits != null) {
+        //   const splitUrl = data.data.splits.uri;
+        //   const splitresponse = await fetch(splitUrl);
+        //   const splitdata = await splitresponse.json();
+        //   setSplits(splitdata.run.splits);
+        // }
         setLoading(false);
       })();
     }
@@ -86,7 +111,6 @@ export default function RunInfo(props) {
       mounted = false;
     };
   }, []);
-
   if (loading) {
     return (
       <ActivityIndicator
@@ -97,60 +121,45 @@ export default function RunInfo(props) {
     );
   } else {
     return (
-      <ScrollView style={styles.container}>
+      <Animated.ScrollView style={{ flex: 1 }} ref={scroll} onScroll={onScroll}>
+        <Modal visible={modalVisible} offset={ScrollY.value} />
         <StatusBar style={"dark"}></StatusBar>
-        {/* <VideoCarousel links={data.videos.links} /> */}
-        <View
-          style={{
-            backgroundColor: "white",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: 20,
-            borderBottomLeftRadius: 20,
-            borderBottomRightRadius: 20,
-            shadowColor: colors.darkgrey,
-            shadowOffset: { width: 5, height: 5 },
-            shadowOpacity: 0.9,
-            elevation: 2,
-          }}
-        >
-          <Text style={h4}>
-            {data.category.data.name} in {timeConverter(data.times.primary)} by{" "}
-            {getPlayers(data)}
-          </Text>
-          <Text style={h4}>n place</Text>
-        </View>
-        <FlatList
-          data={runners}
-          horizontal
-          pagingEnabled
-          showsHorizontalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <View style={{ width, marginVertical: 10 }}>
-              <User username={item.names.international} userid={item.id} />
+        <View style={styles.container}>
+          <TopBar
+            label={"Run Info"}
+            variant={"light"}
+            onPress={onPress}
+            icon={"more-horizontal"}
+          />
+          <VideoCarousel links={data.videos.links} />
+          <Links videolink={data.videos.links[0].uri} weblink={weblink} />
+          <View style={styles.gameinfocontainer}>
+            <GameCard
+              abbreviation={"na"}
+              id={"76rkwed8"}
+              width={100}
+              height={140}
+            />
+            <View style={styles.gameinfo}>
+              <Text style={styles.gameinfotext}>NieR:Automata</Text>
+              <Text style={styles.gameinfotext}>Platforms: PC PS4 XBOX</Text>
+              <Text style={styles.gameinfotext}>Release Date: 2017-02-23</Text>
             </View>
-          )}
-        ></FlatList>
-        <View style={styles.title}>
-          {data.times.ingame == null ? null : (
-            <Text style={h4}>IGT: {timeConverter(data.times.ingame)}</Text>
-          )}
-          {data.times.primary == null ? null : (
-            <Text style={h4}>Primary: {timeConverter(data.times.primary)}</Text>
-          )}
-          {data.times.realtime == null ? null : (
-            <Text style={h4}>
-              Real Time: {timeConverter(data.times.realtime)}
+          </View>
+          <View style={[styles.runtitle, shadow]}>
+            <Runners runners={getPlayers(data)} />
+            <Text style={[h4, styles.padding]}>
+              n place in {timeConverter(data.times.primary)}
             </Text>
-          )}
-          <Text></Text>
-          <Text style={h4}>Submitted: {data.submitted}</Text>
-          <Text style={h4}>Verified by: {data.status.examiner}</Text>
-          <Text style={h4}>Verify-date: {data.status["verify-date"]}</Text>
+            <Text style={[h4, styles.padding]}>
+              Submitted at{" "}
+              {data.submitted.slice(0, data.submitted.indexOf("T"))}
+            </Text>
+            <Text style={[h4, styles.padding]}>{data.comment}</Text>
+          </View>
+          {/* {splits[0] != undefined ? <Splits data={splits} /> : null} */}
         </View>
-
-        {splits[0] != undefined ? <Splits data={splits} /> : null}
-      </ScrollView>
+      </Animated.ScrollView>
     );
   }
 }
@@ -164,12 +173,30 @@ const styles = StyleSheet.create({
     padding: 20,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: colors.white,
     borderRadius: 20,
     margin: 20,
-    shadowColor: colors.darkgrey,
-    shadowOffset: { width: 5, height: 5 },
-    shadowOpacity: 0.9,
-    elevation: 2,
+  },
+  gameinfocontainer: {
+    padding: 20,
+    flexDirection: "row",
+    backgroundColor: colors.light,
+  },
+  gameinfo: {
+    justifyContent: "center",
+    margin: 10,
+    alignItems: "center",
+    flex: 1,
+  },
+  gameinfotext: { fontSize: 17 },
+  runtitle: {
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  padding: {
+    padding: 10,
   },
 });
