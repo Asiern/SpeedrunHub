7432814ab628d2cd94a9f8cd593d2fe16ea602a6
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-12-25 19:12:47 +0100

    updated: Game Interface

--- Source/app/interface/gameInterface.ts
+++ Source/app/interface/gameInterface.ts
@@ -8,4 +8,13 @@ export interface game {
   released: number;
   "release-date": string;
   weblink: string;
+  platforms: string[];
+  ruleset: {
+    "show-milliseconds": boolean;
+    "require-verification": boolean;
+    "require-video": boolean;
+    "run-times": string[];
+    "default-time": string;
+    "emulators-allowed": boolean;
+  };
 }
