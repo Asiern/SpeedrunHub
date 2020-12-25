bb713cfe6fbe9951e1f0b40b3247172bc5936f35
Asier Nuñez <45818936+Asiern@users.noreply.github.com>
2020-12-25 19:12:06 +0100

    added: formatTime + updated: text styles

--- Source/app/components/Run.tsx
+++ Source/app/components/Run.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, StyleSheet, Linking } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-import { colors } from "../themes/theme";
+import { colors, h6, h6p } from "../themes/theme";
 
 export interface RunProps {
   place: string;
@@ -13,21 +13,25 @@ export interface RunProps {
   weblink: string;
 }
 
+function formatTime(time: string) {
+  return time.slice(2, time.length).toLowerCase();
+}
+
 export default function Run({ place, runner, time, weblink }: RunProps) {
   const navigation = useNavigation();
   return (
     <TouchableOpacity
-      onPress={() => Linking.openURL(weblink)}
+      onPress={() => navigation.navigate("RunInfo", { weblink })}
       style={styles.container}
     >
       <View style={styles.place}>
-        <Text style={styles.accenttext}>{place}</Text>
+        <Text style={h6p}>{place}</Text>
       </View>
       <View style={styles.runner}>
-        <Text style={styles.text}>{runner}</Text>
+        <Text style={h6}>{runner}</Text>
       </View>
       <View style={styles.time}>
-        <Text style={styles.text}>{time}</Text>
+        <Text style={h6}>{formatTime(time)}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -37,7 +41,7 @@ const styles = StyleSheet.create({
   container: {
     marginHorizontal: 10,
     marginTop: 10,
-    paddingVertical: 15,
+    paddingVertical: 25,
     flexDirection: "row",
     flex: 1,
     alignItems: "center",
@@ -59,16 +63,5 @@ const styles = StyleSheet.create({
   time: {
     flex: 8,
     alignItems: "center",
-  },
-  text: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    fontSize: 15,
-  },
-  accenttext: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    fontSize: 15,
-    color: colors.primary,
   },
 });
