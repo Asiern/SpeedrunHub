141d8c9478d7896285460902f1f3089d32f5ad5a
Asiern <asiern.dev@gmail.com>
2023-05-01 08:51:33 +0200

    add: getGames hook

--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -3,5 +3,13 @@ import getRuns from "./getRuns";
 import getUser from "./getUser";
 import getUsers from "./getUsers";
 import getPersonalBests from "./getPersonalBests";
+import getGames from "./getGames";
 
-export { getNotifications, getRuns, getUser, getUsers, getPersonalBests };
+export {
+  getNotifications,
+  getRuns,
+  getUser,
+  getUsers,
+  getPersonalBests,
+  getGames,
+};
