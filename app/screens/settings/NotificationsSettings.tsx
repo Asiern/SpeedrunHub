05100429677ca4f1fb6481161a26503ede6fcf05
Asiern <asiern.dev@gmail.com>
2023-05-05 10:17:08 +0200

    updated: Notifications settings

--- app/screens/settings/NotificationsSettings.tsx
+++ app/screens/settings/NotificationsSettings.tsx
@@ -7,11 +7,12 @@ import {
   ToastAndroid,
   Switch,
 } from "react-native";
-import { h3, h4b, h6 } from "../../themes/theme";
-import { config, context } from "../../config/config";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { context } from "../../config/config";
 import Slider from "@react-native-community/slider";
-import Button from "../../components/Buttons/Button";
+import { config } from "../../types";
+import Constants from "expo-constants";
+import { SquareButton } from "../../components/SquareButton";
+import { useNavigation } from "@react-navigation/native";
 const { width } = Dimensions.get("window");
 
 async function apply(
@@ -21,48 +22,59 @@ async function apply(
   setConfig: (config: config) => void
 ) {
   try {
-    config.notifications.max = max;
-    config.notifications.unread = unread;
-    setConfig(config);
-    await AsyncStorage.setItem("@Config", JSON.stringify(config));
+    const notifications = config.notifications;
+    setConfig({ ...config, notifications: { ...notifications, max, unread } });
     showToastWithGravity("Settings applied correctly");
   } catch (error) {
     showToastWithGravity(error);
   }
 }
+
 const showToastWithGravity = (text: string) => {
   ToastAndroid.showWithGravity(text, ToastAndroid.SHORT, ToastAndroid.CENTER);
 };
 
-export function NotificationsSettings() {
-  const { Config, setConfig, theme } = useContext(context);
-  const [max, setMax] = useState<number>(Config.notifications.max);
-  const [unread, setUnread] = useState<boolean>(Config.notifications.unread);
+export function NotificationsSettings(): JSX.Element {
+  // Retrieve the theme from the app context
+  const { config, setConfig } = useContext(context)!;
+  const { theme } = config;
+  const navigation = useNavigation();
+  const [max, setMax] = useState<number>(config.notifications.max);
+  const [unread, setUnread] = useState<boolean>(config.notifications.unread);
   return (
     <View
       style={[styles.container, { backgroundColor: theme.colors.background }]}
     >
-      <View style={[styles.section, { backgroundColor: theme.colors.card }]}>
+      <View
+        style={[styles.section, { backgroundColor: theme.colors.foreground }]}
+      >
         <View style={styles.row}>
-          <Text style={[h4b, { color: theme.colors.text }]}>
+          <Text style={[{ color: theme.colors.headerText }, styles.text]}>
             Only show unread notifications
           </Text>
           <Switch
             value={unread}
             onValueChange={() => setUnread(!unread)}
             thumbColor={theme.colors.primary}
-            trackColor={theme.colors.text}
-            ios_backgroundColor={theme.colors.text}
+            trackColor={theme.colors.headerText}
+            ios_backgroundColor={theme.colors.background}
           />
         </View>
       </View>
-      <View style={[styles.section, { backgroundColor: theme.colors.card }]}>
-        <Text style={[h4b, { color: theme.colors.text }]}>
+      <View
+        style={[styles.section, { backgroundColor: theme.colors.foreground }]}
+      >
+        <Text style={[{ color: theme.colors.headerText }, styles.text]}>
           Set the number of notifications to receive{"\n"}
         </Text>
-        <Text style={[h3, { color: theme.colors.text }]}>{max}</Text>
+        <Text style={[{ color: theme.colors.headerText }, styles.text]}>
+          {max}
+        </Text>
         <Slider
-          style={{ width: width - 40, height: 40 }}
+          style={{
+            width: width - 40,
+            height: 40,
+          }}
           minimumValue={20}
           maximumValue={200}
           minimumTrackTintColor={theme.colors.primary}
@@ -71,38 +83,53 @@ export function NotificationsSettings() {
           onValueChange={(value) => setMax(Math.round(value))}
         />
       </View>
-      {Config.user.key.length > 0 ? null : (
+      {config.key ? (
+        <SquareButton
+          icon="save"
+          onPress={() => apply(config, max, unread, setConfig)}
+        />
+      ) : (
         <View
           style={{
-            padding: 20,
+            padding: 30,
             alignItems: "center",
             justifyContent: "center",
           }}
         >
-          <Text style={h6}>
+          <Text
+            style={[
+              styles.text,
+              {
+                color: theme.colors.headerText,
+                textAlign: "justify",
+                marginBottom: 10,
+              },
+            ]}
+          >
             It seems you did not provide your api key, the key is required to
-            receive notifications.{"\n"}If you want to receive notifications,
-            log in with your key.
+            receive notifications.{"\n"}Please log back in using your key.
           </Text>
+          <SquareButton
+            icon="log-out"
+            onPress={() => navigation.navigate("Login")}
+          />
         </View>
       )}
-      <Button
-        variant={"primary"}
-        label={"Apply Changes"}
-        onPress={() => apply(Config, max, unread, setConfig)}
-        width={(width / 3) * 2}
-      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center" },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: Constants.statusBarHeight,
+  },
   section: {
     width,
     justifyContent: "center",
     alignItems: "center",
-    padding: 20,
+    padding: 30,
     marginBottom: 10,
   },
   row: {
@@ -110,5 +137,9 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: "center",
     width,
+  },
+  text: {
+    fontFamily: "Poppins",
+    fontSize: 14,
   },
 });
