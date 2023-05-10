d715ee07ac643eaa9395e0b8a1d61045cca026d0
Asiern <asiern.dev@gmail.com>
2023-05-10 19:04:41 +0200

    chore: usercard tests

---
+++ __tests__/components/UserCard/UserCard.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { UserCard } from "../../../app/components/UserCard";
+import { userData } from "../../../__mocks__/MockUserData";
+import { useConfig } from "../../../app/hooks";
+import { defaultConfig } from "../../../app/config/config";
+import { render } from "react-native-testing-library";
+
+jest.mock("../../../app/hooks/useConfig");
+
+beforeEach(() => {
+  useConfig.mockReturnValue({ config: defaultConfig, setConfig: jest.fn() });
+});
+
+const renderComponent = () => {
+  return <UserCard user={userData} />;
+};
+
+describe("UserCard component", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(renderComponent()).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("renders username correctly", () => {
+    const { getByText } = render(renderComponent());
+    expect(getByText(userData.names.international)).toBeDefined();
+  });
+  it("renders country correctly", () => {
+    const { getByText } = render(renderComponent());
+    expect(
+      getByText(userData.location.country.names.international)
+    ).toBeDefined();
+  });
+});
