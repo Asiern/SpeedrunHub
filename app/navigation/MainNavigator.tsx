c26a2112ed3890606e7873a03234d8140487adfc
Asiern <asiern.dev@gmail.com>
2023-05-21 16:50:21 +0200

    chore: add notification button to home screen + update tests

--- app/navigation/MainNavigator.tsx
+++ app/navigation/MainNavigator.tsx
@@ -6,11 +6,12 @@ import Home from "../screens/home/Home";
 import { Settings } from "../screens/settings";
 import GameInfo from "../screens/GameInfo";
 import Search from "../screens/search/Search";
-import Notifications from "../screens/Notifications";
+import { NotificationsContainer } from "../screens/notifications";
 import RunInfo from "../screens/RunInfo";
 import { About, NotificationsSettings } from "../screens/settings";
 import { user } from "../hooks/types";
 import { Profile } from "../screens/profile";
+import GameList from "../screens/GameList";
 
 const Stack = createStackNavigator();
 
@@ -44,13 +45,18 @@ function HomeStack(): JSX.Element {
         options={{ headerShown: false }}
       />
       <Stack.Screen
+        name="GameList"
+        component={GameList}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
         name="GameInfo"
         component={GameInfo}
         options={{ headerShown: false }}
       />
       <Stack.Screen
         name="Notifications"
-        component={Notifications}
+        component={NotificationsContainer}
         options={{ headerShown: false }}
       />
       <Stack.Screen
