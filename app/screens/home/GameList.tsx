2398f11bcc628084cc3248d1411e6f3aad9ed819
Asiern <asiern.dev@gmail.com>
2023-05-30 17:08:03 +0200

    chore: add empty list component

--- app/screens/home/GameList.tsx
+++ app/screens/home/GameList.tsx
@@ -29,7 +29,7 @@ const SLIDE_WIDTH: number = width - 2 * 30;
 
 function GameList(): JSX.Element {
   const { config } = useConfig();
-  const { games, theme } = config;
+  const { theme, games } = config;
   const navigation = useNavigation();
   const gamesToRender: number = Math.min(games.length, PREVIEW_CARD_COUNT);
   const N_SLIDES: number = Math.ceil(gamesToRender / N_CARDS_SLIDE);
@@ -50,68 +50,94 @@ function GameList(): JSX.Element {
   return (
     <View style={styles.container}>
       <Text
+        testID="header-title"
         style={[styles.headerText, { color: theme.colors.primary }]}
         onPress={() => navigation.navigate("GameList")}
       >
         My Games
       </Text>
-      <Animated.ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        decelerationRate={"fast"}
-        snapToInterval={SLIDE_WIDTH + CARD_GAP}
-        bounces={false}
-        scrollEventThrottle={16}
-        onScroll={onScroll}
-      >
-        {games
-          .slice(0, gamesToRender)
-          .map(({ abbreviation, id, uri }: game, i: number) => {
-            return (
-              <GameCard
-                style={{
-                  marginRight: i === gamesToRender - 1 ? 0 : CARD_GAP,
-                  marginBottom: 3,
-                }}
-                {...{
-                  abbreviation,
-                  id,
-                  image: uri,
-                  width: CARD_WIDTH,
-                  height: CARD_HEIGHT,
-                }}
-                key={id}
-              />
-            );
-          })}
-        {gamesToRender < games.length ? (
-          <TouchableOpacity
-            style={[
-              styles.moreCard,
-              shadow,
-              {
-                backgroundColor: theme.colors.primary,
-              },
-            ]}
-            onPress={() => navigation.navigate("GameList")}
-            testID="more-card-touchable"
-          >
-            <Feather name="list" color={theme.colors.foreground} size={25} />
-            <Text style={[styles.text, { color: theme.colors.foreground }]}>
-              Show All
-            </Text>
-          </TouchableOpacity>
-        ) : nFillerCards > 0 ? (
-          <View
+      {games.length === 0 ? (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Search", { query: "" })}
+          testID="list-empty-touchable"
+          style={[
+            styles.listEmptyCard,
+            {
+              backgroundColor: theme.colors.primary,
+            },
+            shadow,
+          ]}
+        >
+          <Text
             style={{
-              width:
-                nFillerCards === 1
-                  ? CARD_WIDTH + CARD_GAP
-                  : 2 * CARD_WIDTH + 2 * CARD_GAP,
+              fontFamily: "Poppins",
+              color: theme.colors.foreground,
+              textAlign: "center",
             }}
-          />
-        ) : null}
-      </Animated.ScrollView>
+          >
+            Your liked games list is empty. Add your favorite games now!
+          </Text>
+        </TouchableOpacity>
+      ) : (
+        <Animated.ScrollView
+          testID="game-cards-container"
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          decelerationRate={"fast"}
+          snapToInterval={SLIDE_WIDTH + CARD_GAP}
+          bounces={false}
+          scrollEventThrottle={16}
+          onScroll={onScroll}
+        >
+          {games
+            .slice(0, gamesToRender)
+            .map(({ abbreviation, id, uri }: game, i: number) => {
+              return (
+                <GameCard
+                  style={{
+                    marginRight: i === gamesToRender - 1 ? 0 : CARD_GAP,
+                    marginBottom: 3,
+                  }}
+                  {...{
+                    abbreviation,
+                    id,
+                    image: uri,
+                    width: CARD_WIDTH,
+                    height: CARD_HEIGHT,
+                  }}
+                  key={id}
+                />
+              );
+            })}
+          {gamesToRender < games.length ? (
+            <TouchableOpacity
+              style={[
+                styles.moreCard,
+                shadow,
+                {
+                  backgroundColor: theme.colors.primary,
+                },
+              ]}
+              onPress={() => navigation.navigate("GameList")}
+              testID="more-card-touchable"
+            >
+              <Feather name="list" color={theme.colors.foreground} size={25} />
+              <Text style={[styles.text, { color: theme.colors.foreground }]}>
+                Show All
+              </Text>
+            </TouchableOpacity>
+          ) : nFillerCards > 0 ? (
+            <View
+              style={{
+                width:
+                  nFillerCards === 1
+                    ? CARD_WIDTH + CARD_GAP
+                    : 2 * CARD_WIDTH + 2 * CARD_GAP,
+              }}
+            />
+          ) : null}
+        </Animated.ScrollView>
+      )}
       <ScrollIndicator
         gap={CARD_GAP / 2}
         index={currentIndex}
@@ -142,6 +168,11 @@ const styles = StyleSheet.create({
   },
   text: {
     fontFamily: "Poppins",
+  },
+  listEmptyCard: {
+    width: SLIDE_WIDTH,
+    borderRadius: 10,
+    padding: 10,
   },
 });
 
