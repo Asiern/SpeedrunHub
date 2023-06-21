1f75fc0e0ae797f7543e6160a412d8d18b458d33
Asiern <asiern.dev@gmail.com>
2023-06-21 21:33:05 +0200

    chore: get users through leaderboard request

--- app/screens/game/Leaderboard/Leaderboard.tsx
+++ app/screens/game/Leaderboard/Leaderboard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { leaderboard } from "../../../hooks/types";
+import { leaderboard, user } from "../../../hooks/types";
 import { Run } from "../../../components";
 
 interface ILeaderboard {
@@ -12,7 +12,28 @@ function Leaderboard({ leaderboard }: ILeaderboard): JSX.Element {
   return (
     <View style={styles.container}>
       {runs.map(({ run, place }) => {
-        return <Run key={run.id} {...{ run, place }} />;
+        let players: user[] | undefined = undefined;
+        run.players.forEach((player) => {
+          const playerData = leaderboard.players?.data.find(
+            (p) => p.id === player.id
+          );
+          if (playerData) {
+            if (!players) players = [playerData];
+            else players.push(playerData);
+          }
+        });
+        leaderboard.players?.data.find((p) => p.id === run.players[0].id);
+        if (!players) return null;
+        return (
+          <Run
+            key={run.id}
+            {...{
+              run,
+              place,
+              players,
+            }}
+          />
+        );
       })}
     </View>
   );
@@ -23,4 +44,5 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
   },
 });
+
 export default Leaderboard;
