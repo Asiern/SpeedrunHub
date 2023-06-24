fc9881bdcaec80eba4bd6367b2b4684b26a99be9
Asiern <asiern.dev@gmail.com>
2023-06-24 16:13:36 +0200

    chore: optimized run for memoization

--- __tests__/components/Run/Run.test.js
+++ __tests__/components/Run/Run.test.js
@@ -6,7 +6,6 @@ import { useConfig } from "../../../app/hooks";
 import { defaultConfig } from "../../../app/config/config";
 import { useNavigation } from "@react-navigation/native";
 import { fireEvent } from "react-native-testing-library";
-import { userData } from "../../../__mocks__/MockUserData";
 
 jest.mock("../../../app/hooks/useConfig");
 
@@ -17,14 +16,37 @@ beforeAll(() => {
 describe("Run component", () => {
   it("displays place correctly", async () => {
     const { getByText } = render(
-      <Run place={20} run={runData} players={[userData]} />
+      <Run
+        place={20}
+        players={"Player"}
+        time="1h22m5s"
+        weblink={runData.weblink}
+      />
     );
     const place = getByText("20");
     expect(place).toBeDefined();
   });
+  it("displays time correctly", async () => {
+    const time = "1h22m5s";
+    const { getByText } = render(
+      <Run
+        place={20}
+        players={"Player"}
+        time={time}
+        weblink={runData.weblink}
+      />
+    );
+    const timeComp = getByText(time);
+    expect(timeComp).toBeDefined();
+  });
   it("navigates to RunInfo screen when pressed", () => {
     const { getByTestId } = render(
-      <Run run={runData} place={0} players={[userData]} />
+      <Run
+        place={20}
+        players={"Player"}
+        time="1h22m5s"
+        weblink={runData.weblink}
+      />
     );
     const touchable = getByTestId("run-touchable");
     fireEvent.press(touchable);
