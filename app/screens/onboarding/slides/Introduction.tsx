a124d5c9102d28872f23586d823fedc71445029c
Asiern <asiern.dev@gmail.com>
2023-07-03 13:05:11 +0200

    chore: onboarding locales

--- app/screens/onboarding/slides/Introduction.tsx
+++ app/screens/onboarding/slides/Introduction.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useConfig } from "../../../hooks";
+import { useTranslation } from "react-i18next";
 
 interface IIntroduction {
   width: number;
@@ -8,6 +9,7 @@ interface IIntroduction {
 export default function Introduction({ width }: IIntroduction): JSX.Element {
   const { config } = useConfig();
   const { theme } = config;
+  const { t } = useTranslation();
   return (
     <View style={[styles.container, { width }]}>
       <Text style={[styles.header, { color: theme.colors.primary }]}>
@@ -19,7 +21,7 @@ export default function Introduction({ width }: IIntroduction): JSX.Element {
           { color: theme.colors.headerText, marginTop: 5 },
         ]}
       >
-        Welcome to the SpeedrunHub!
+        {t("welcome", { ns: "glossary" })}
       </Text>
       <Text
         style={[
@@ -27,13 +29,7 @@ export default function Introduction({ width }: IIntroduction): JSX.Element {
           { color: theme.colors.headerText, textAlign: "justify" },
         ]}
       >
-        Explore the exciting world of speedrunning and discover the fastest
-        gameplay records from speedrun.com. Stay updated with the latest
-        leaderboard rankings, watch incredible speedrun videos, and follow your
-        favorite games and runners. Join us on this thrilling journey through
-        the world of speedrunning and witness gaming history in action. Get
-        ready to experience the adrenaline of speedruns like never before. Let
-        {"\u2018"}s race!
+        {t("screens.onboarding.introduction")}
       </Text>
     </View>
   );
